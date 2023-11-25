6d64c24980a47183491a71b6cb4767356d97d55f
Ranamalsingh12 <ranams99911@gmail.com>
2023-11-25 15:42:44 +0530

    fixing all the css bugs

--- src/components/My3Dcomponent.jsx
+++ src/components/My3Dcomponent.jsx
@@ -1,20 +1,31 @@
-import React, { useEffect } from 'react';
-import { Application } from '@splinetool/runtime';
+import React, { useEffect } from "react";
+import { Application } from "@splinetool/runtime";
 
 const My3Dcomponent = () => {
   useEffect(() => {
-
-    const canvas = document.getElementById('canvas3d');
+    const canvas = document.getElementById("canvas3d");
 
     if (canvas) {
       const app = new Application(canvas);
-      app.load('https://prod.spline.design/eehaN3kDSPoWISYR/scene.splinecode');
+      app.load("https://prod.spline.design/lI0Ti84haSYtQMvM/scene.splinecode");
+      // https://prod.spline.design/lI0Ti84haSYtQMvM/scene.splinecode foam
+      // app.load('https://prod.spline.design/xMg2fmy-xOz2dZLG/scene.splinecode'); // wave
+      // app.load('https://prod.spline.design/o90mkX-7JF9wGltL/scene.splinecode'); Earth
+      // app.load('https://prod.spline.design/eehaN3kDSPoWISYR/scene.splinecode'); // first one
+
+      return () => {
+        if (app && typeof app.unload === "function") {
+          app.unload();
+        }
+
+        app.dispose();
+      };
     }
   }, []);
 
   return (
     <div>
-      <canvas id="canvas3d" width="1200" height="700"></canvas>
+      <canvas id="canvas3d" width="800" height="600"></canvas>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,50 @@
-import React from 'react';
-import '../css/Navbar.css';
+import React, { useState, useEffect } from "react";
+import "../css/Navbar.css";
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
+
+  const transitionNavbar = (e) => {
+    if (window.scrollY) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavbar);
+    return () => {
+      window.removeEventListener("scroll", transitionNavbar);
+    };
+  }, []);
+
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${show && "nav_black"}`}>
       <div className="logo">
         <img src="logo.png" alt="Logo" />
       </div>
       <div className="menu">
         <ul>
-          <li><a href="#about">About</a></li>
-          <li><a href="#themes">Themes</a></li>
-          <li><a href="#sponsors">Sponsors</a></li>
-          <li><a href="#team">Team</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#themes">Themes</a>
+          </li>
+          <li>
+            <a href="#sponsors">Sponsors</a>
+          </li>
+          <li>
+            <a href="#team">Team</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
         </ul>
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
