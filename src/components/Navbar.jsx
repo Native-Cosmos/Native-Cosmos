d2c29382380c125293a60e8e778af8bdb285307b
Ranamalsingh12 <ranams99911@gmail.com>
2024-01-28 23:19:20 +0530

    map issue fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,54 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import "../css/Navbar.css";
-
-// const Navbar = () => {
-//   const [show, setShow] = useState(false);
-
-//   const transitionNavbar = (e) => {
-//     if (window.scrollY) {
-//       setShow(true);
-//     } else {
-//       setShow(false);
-//     }
-//   };
-
-//   useEffect(() => {
-//     window.addEventListener("scroll", transitionNavbar);
-//     return () => {
-//       window.removeEventListener("scroll", transitionNavbar);
-//     };
-//   }, []);
-
-//   return (
-//     <nav className={`navbar ${show && "nav_black"}`}>
-//       <div className="logo">
-//         <img src="nativecosmos.png" alt="Logo" />
-//       </div>
-//       <div className="menu">
-//         <ul>
-//           <li>
-//             <a href="#about">About</a>
-//           </li>
-//           <li>
-//             <a href="#themes">Themes</a>
-//           </li>
-//           <li>
-//             <a href="#sponsors">Sponsors</a>
-//           </li>
-//           <li>
-//             <a href="#team">Team</a>
-//           </li>
-//           <li>
-//             <a href="#contact">Contact</a>
-//           </li>
-//         </ul>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
 import React, { useState, useEffect } from "react";
 import "../css/Navbar.css";
 
@@ -57,7 +6,7 @@ const Navbar = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      const isScrolled = window.scrollY > 10; // Adjust the scroll threshold as needed
+      const isScrolled = window.scrollY > 10;
       setScrolled(isScrolled);
     };
 
