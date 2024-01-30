89ba53717c50a79ccef1e85342892e08672a0769
Ranamalsingh12 <ranams99911@gmail.com>
2024-01-30 12:54:12 +0530

    updations

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="footer-content">
-        <h2>Made with ❤️ by us</h2>
-        <h4>&copy; Us 2024</h4>
+        <h3>Made with ❤️ by us</h3>
+        <h3>&copy; Us 2024</h3>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import "../css/Navbar.css";
 
 const Navbar = () => {
@@ -10,45 +11,60 @@ const Navbar = () => {
       setScrolled(isScrolled);
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
-
-
   return (
-<>
-    <nav className={`navbar ${scrolled ? 'scrolled' : ''}`}>
-
-      <div className="logo">
-        <img src="nativecosmos.png" alt="Logo" />
-      </div>
-      <div className="menu">
-        <ul>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#themes">Themes</a>
-          </li>
-          <li>
-            <a href="#sponsors">Sponsors</a>
-          </li>
-          <li>
-            <a href="#team">Team</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <>
+      <nav className={`navbar ${scrolled ? "scrolled" : ""}`}>
+        <div className="logo">
+          <img
+            style={{ borderRadius: "50%" }}
+            src="nativecosmos.png"
+            alt="Logo"
+          />
+        </div>
+        <div className="menu">
+          <ul>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#themes">Themes</a>
+            </li>
+            <li>
+              <a href="#sponsors">Sponsors</a>
+            </li>
+            <li>
+              <a href="#team">Team</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+            {/* <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/team">Team</Link>
+            </li>
+            <li>
+              <Link to="/faq">FAQ</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li> */}
+          </ul>
+        </div>
+      </nav>
     </>
   );
 };
 
 export default Navbar;
-﻿
